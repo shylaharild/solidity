6d86a27e5c5efc0003ee797893957fffb0948599
Daniel Kirchner <daniel@ekpyron.org>
2018-11-15 13:35:54 +0100

    Move emscripten flags.

--- scripts/travis-emscripten/build_emscripten.sh
+++ scripts/travis-emscripten/build_emscripten.sh
@@ -91,7 +91,6 @@ cmake \
   -DBoost_SYSTEM_LIBRARY_RELEASE="$WORKSPACE"/boost_1_67_0/libboost_system.a \
   -DBoost_UNIT_TEST_FRAMEWORK_LIBRARY_RELEASE="$WORKSPACE"/boost_1_67_0/libboost_unit_test_framework.a \
   -DTESTS=0 \
-  -DCMAKE_EXE_LINKER_FLAGS="-s \"EXTRA_EXPORTED_RUNTIME_METHODS=['ccall', 'cwrap', 'Runtime', 'addFunction', 'removeFunction', 'Pointer_stringify', 'lengthBytesUTF8', '_malloc', 'stringToUTF8', 'setValue']\" -s WASM=0" \
   ..
 make -j 4
 
